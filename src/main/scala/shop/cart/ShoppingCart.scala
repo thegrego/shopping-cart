76dc53fc90d95ad79cdb325dd137bbcae1e66d00
Grzegorz (Gregory) Dobrowolski <dobrowolski.grzegorz@gmail.com>
2016-10-11 23:02:22 +0100

    Solution for Step 2

--- src/main/scala/shop/cart/ShoppingCart.scala
+++ src/main/scala/shop/cart/ShoppingCart.scala
@@ -1,12 +1,19 @@
 package shop.cart
 
 class ShoppingCart {
-  def price(fruits: List[String]) = fruits.map(
-    fruit => fruit match {
-      case "Apple" => 0.6
-      case "Orange" => 0.25
-      case _ => 0
-    }
-  ).sum
+  def price(fruits: List[String], offers: Map[String, Tuple2[Int, Int]] = Map("" -> (1, 1))) =
+    fruits
+      .groupBy(x => x)
+      .map(
+        fruit => fruit match {
+          case ("Apple", list) => offer(list.size, offers.get("Apple"), 0.6)
+          case ("Orange", list) => offer(list.size, offers.get("Orange"), 0.25)
+          case _ => 0
+        }
+      ).sum
 
+  private def offer(amount: Int, offerDetails: Option[Tuple2[Int, Int]], price: Double): Double = {
+    val (offerRate, priceRate) = offerDetails.getOrElse((1, 1))
+    amount / offerRate * priceRate * price + amount % offerRate * price
+  }
 }
--- src/test/scala/shop/cart/ShoppingCartSpec.scala
+++ src/test/scala/shop/cart/ShoppingCartSpec.scala
@@ -4,10 +4,23 @@ import org.scalatest.FlatSpec
 
 class ShoppingCartSpec extends FlatSpec {
   "Total price for aplles and orange" should "be 2.05" in {
-    val fruits = List("Apple", "Apple", "Orange", "Apple")
     val shoppingCart = new ShoppingCart
+    val fruits = List("Apple", "Apple", "Orange", "Apple")
 
     assert(shoppingCart.price(fruits) === 2.05)
   }
 
+  "Total price after offers for apples and oranges" should "be 2.2" in {
+    val fruits = List("Apple", "Apple",  "Apple", "Orange", "Orange", "Orange", "Orange", "Orange")
+    val shoppingCart = new ShoppingCart
+    val offers: Map[String, Tuple2[Int, Int]] = Map("Apple" -> (2, 1), "Orange" -> (3, 2))
+    assert(shoppingCart.price(fruits, offers) === 2.2)
+  }
+
+  "Total price after offers for apples and oranges" should "be 1.1" in {
+    val fruits = List("Apple", "Orange", "Orange")
+    val shoppingCart = new ShoppingCart
+    val offers: Map[String, Tuple2[Int, Int]] = Map("Apple" -> (2, 1), "Orange" -> (3, 2))
+    assert(shoppingCart.price(fruits, offers) === 1.1)
+  }
 }
